c2248d6d1f1e8c58046c4cc781e2e36d3a2c29b1
dfhns967 <65913927+dfhns967@users.noreply.github.com>
2020-08-20 19:46:28 +0300

    Update DMall.js

--- commands/dms/DMall.js
+++ commands/dms/DMall.js
@@ -80,7 +80,7 @@ class DMallCommand extends commando.Command {
                 console.log(`Waited ${timeout}ms.\t|${i + 1}|\tDMing ${member.user.username}`);
             }
             try {
-                member.send(`${msg} \n`);
+                member.send(`${msg} \n ${m})`);
                 successcount++;
             } catch (error) {
                 console.log(`Failed to send DM! ` + error)
